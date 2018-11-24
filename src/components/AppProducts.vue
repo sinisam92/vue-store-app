cdc0b5db687109b83f9810eba145bc014318e150
sinisa <sinisamanojlovic1@gmail.com>
2018-11-24 20:16:57 +0100

    Zadatak 7

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -3,15 +3,19 @@
         <input type="text" placeholder="Search" v-model="search">
         <table border="1" class="table">
             <thead>
-                <td>Id - number</td>
-                <td>Title</td>
-                <td>Quantity</td>
+                <th>Id - number</th>
+                <th>Title</th>
+                <th>Quantity</th>
+                
             </thead>
             <tbody>
                 <tr v-for="(product, key) in products" :key="key" v-if="product.title.toLowerCase().includes(search.toLowerCase())">
                     <td>{{ product.id }}</td>
                     <td>{{ product.title }}</td>
                     <td>{{ product.quantity }}</td>
+
+                    <button class="btn btn-success" @click="addProduct(product)" type="submit">+</button>
+                    <button class="btn btn-danger" @click="sellProduct(product)" type="submit">-</button>
                 </tr>
             </tbody>
         </table>
@@ -27,6 +31,14 @@ export default {
         return {
             products: ProductService.list(),
             search: ''
+        }
+    },
+    methods: {
+        addProduct(product) {
+            ProductService.increment(product)
+        },
+        sellProduct(product) {
+            ProductService.decrement(product)
         }
     }
 }
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,12 +1,12 @@
 const products = [
-    {id: 1, title: 'Jabuka', quantity: 10},
-    {id: 2, title: 'Kruska', quantity: 25},
-    {id: 3, title: 'Pomorandza', quantity: 67},
-    {id: 4, title: 'Kivi', quantity: 23},
-    {id: 5, title: 'Sljive', quantity: 45},
-    {id: 6, title: 'Nar', quantity: 23},
-    {id: 7, title: 'Lubenice', quantity: 62},
-    {id: 8, title: 'Banane', quantity: 23},
+    { id: 1, title: 'Jabuka', quantity: 10 },
+    { id: 2, title: 'Kruska', quantity: 25 },
+    { id: 3, title: 'Pomorandza', quantity: 67 },
+    { id: 4, title: 'Kivi', quantity: 23 },
+    { id: 5, title: 'Sljive', quantity: 45 },
+    { id: 6, title: 'Nar', quantity: 23 },
+    { id: 7, title: 'Lubenice', quantity: 62 },
+    { id: 8, title: 'Banane', quantity: 23 },
 ];
 
 class ProductService {
@@ -17,7 +17,8 @@ class ProductService {
         return products.find(product => product.id == id);
     }
     increment(product) {
-        product.quantity++
+        product.quantity++  
+        
     }
     
     decrement(product) {
